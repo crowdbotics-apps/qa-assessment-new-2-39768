d8e2fe43f4154555c4e9c9c75bda37b41da74fcb
Andre Machado <andre.machado@crowdbotics.com>
2023-04-06 19:07:55 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "qa-assessment-new-2-39768",
   url: "https://qa-assessment-new-2-39768.botics.co",
   api: "https://qa-assessment-new-2-39768.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
