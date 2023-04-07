babbb82df1ac80b98797e587a72359223d9bff1d
crowdbotics-apps <team@crowdbotics.com>
2023-04-07 00:17:15 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,14 @@
+from django.conf import settings
 from django.db import models
+class Profile(models.Model):
+    'Generated Model'
+    first_name = models.CharField(max_length=200,)
+    last_name = models.CharField(max_length=200,)
+    email = models.EmailField(max_length=254,)
+    date_of_birth = models.DateField()
+    address = models.TextField()
+    city = models.CharField(max_length=200,)
+    state = models.CharField(max_length=200,)
+    zip_code = models.CharField(max_length=200,)
+    country = models.CharField(max_length=200,)
+    user = models.ForeignKey("users.User",on_delete=models.CASCADE,related_name="profile_user",)
