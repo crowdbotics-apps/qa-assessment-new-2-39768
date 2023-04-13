3a06b4be4a617c4d3c9c62d29570f55435e3d7d8
crowdbotics-apps <team@crowdbotics.com>
2023-04-13 01:23:27 +0000

    Update models

--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -1,4 +1,4 @@
-from .models import Coin, MetalType
+from .models import Coin, MetalType, OfferList, Offer
 from django.views.generic import CreateView, UpdateView, DetailView, ListView
 from django.shortcuts import render
 
