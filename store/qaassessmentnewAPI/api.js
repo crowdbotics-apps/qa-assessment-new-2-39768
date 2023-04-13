c86acf686d06ac0c4d4dabf91d14c7f2b675d6da
Crowdbotics <engineering@crowdbotics.com>
2023-04-13 00:47:04 +0000

    Update store.

--- store/qaassessmentnewAPI/api.js
+++ store/qaassessmentnewAPI/api.js
@@ -1,96 +1,82 @@
-import axios from "axios";
+import axios from "axios"
 const qaassessmentnewAPI = axios.create({
   baseURL: "https://qa-assessment-new-2-39768.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_v1_login_create(payload) {
-  return qaassessmentnewAPI.post(`/api/v1/login/`, payload.data);
+  return qaassessmentnewAPI.post(`/api/v1/login/`, payload.data)
 }
-
 function api_v1_signup_create(payload) {
-  return qaassessmentnewAPI.post(`/api/v1/signup/`, payload.data);
+  return qaassessmentnewAPI.post(`/api/v1/signup/`, payload.data)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return qaassessmentnewAPI.get(`/rest-auth/user/`);
+  return qaassessmentnewAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return qaassessmentnewAPI.put(`/rest-auth/user/`, payload.data);
+  return qaassessmentnewAPI.put(`/rest-auth/user/`, payload.data)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return qaassessmentnewAPI.patch(`/rest-auth/user/`, payload.data);
+  return qaassessmentnewAPI.patch(`/rest-auth/user/`, payload.data)
 }
-
 function api_docs_schema_retrieve(payload) {
   return qaassessmentnewAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function rest_auth_login_create(payload) {
-  return qaassessmentnewAPI.post(`/rest-auth/login/`, payload.data);
+  return qaassessmentnewAPI.post(`/rest-auth/login/`, payload.data)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return qaassessmentnewAPI.get(`/rest-auth/logout/`);
+  return qaassessmentnewAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return qaassessmentnewAPI.post(`/rest-auth/logout/`);
+  return qaassessmentnewAPI.post(`/rest-auth/logout/`)
 }
-
 function modules_privacy_policy_list(payload) {
-  return qaassessmentnewAPI.get(`/modules/privacy-policy/`);
+  return qaassessmentnewAPI.get(`/modules/privacy-policy/`)
 }
-
 function modules_privacy_policy_create(payload) {
-  return qaassessmentnewAPI.post(`/modules/privacy-policy/`, payload.data);
+  return qaassessmentnewAPI.post(`/modules/privacy-policy/`, payload.data)
 }
-
 function rest_auth_registration_create(payload) {
-  return qaassessmentnewAPI.post(`/rest-auth/registration/`, payload.data);
+  return qaassessmentnewAPI.post(`/rest-auth/registration/`, payload.data)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return qaassessmentnewAPI.post(`/rest-auth/password/reset/`, payload.data);
+  return qaassessmentnewAPI.post(`/rest-auth/password/reset/`, payload.data)
 }
-
 function rest_auth_password_change_create(payload) {
-  return qaassessmentnewAPI.post(`/rest-auth/password/change/`, payload.data);
+  return qaassessmentnewAPI.post(`/rest-auth/password/change/`, payload.data)
 }
-
 function modules_privacy_policy_retrieve(payload) {
-  return qaassessmentnewAPI.get(`/modules/privacy-policy/${payload.id}/`);
+  return qaassessmentnewAPI.get(`/modules/privacy-policy/${payload.id}/`)
 }
-
 function modules_privacy_policy_update(payload) {
-  return qaassessmentnewAPI.put(`/modules/privacy-policy/${payload.id}/`, payload.data);
+  return qaassessmentnewAPI.put(
+    `/modules/privacy-policy/${payload.id}/`,
+    payload.data
+  )
 }
-
 function modules_privacy_policy_partial_update(payload) {
-  return qaassessmentnewAPI.patch(`/modules/privacy-policy/${payload.id}/`, payload.data);
+  return qaassessmentnewAPI.patch(
+    `/modules/privacy-policy/${payload.id}/`,
+    payload.data
+  )
 }
-
 function modules_privacy_policy_destroy(payload) {
-  return qaassessmentnewAPI.delete(`/modules/privacy-policy/${payload.id}/`);
+  return qaassessmentnewAPI.delete(`/modules/privacy-policy/${payload.id}/`)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return qaassessmentnewAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
+  return qaassessmentnewAPI.post(
+    `/rest-auth/password/reset/confirm/`,
+    payload.data
+  )
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return qaassessmentnewAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
+  return qaassessmentnewAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    payload.data
+  )
 }
-
 export const apiService = {
   api_v1_login_create,
   api_v1_signup_create,
@@ -112,4 +98,4 @@ export const apiService = {
   modules_privacy_policy_destroy,
   rest_auth_password_reset_confirm_create,
   rest_auth_registration_verify_email_create
-};
+}
