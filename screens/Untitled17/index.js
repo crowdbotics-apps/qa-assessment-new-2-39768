35b59ca15886aa45a5250ff8f1a8604050c76b19
Saad bin Abid <saad.abid@crowdbotics.com>
2023-05-31 12:55:10 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled17/index.js
+++ screens/Untitled17/index.js
@@ -1,20 +1,65 @@
 import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
 
-const Untitled17 = () => {
-  return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
-      padding: 10,
-      position: 'relative',
-      flex: 1
-    }}></ScrollView>
-    </SafeAreaView>;
+const Card = () => {
+  const data = [{
+    id: "1",
+    name: "Test Name 1",
+    quantity: 3,
+    price: 45
+  }, {
+    id: "2",
+    name: "Test Name 2",
+    quantity: 2,
+    price: 30
+  }, {
+    id: "3",
+    name: "Test Name 3",
+    quantity: 1,
+    price: 15
+  }];
+
+  const renderItem = ({
+    item
+  }) => <View style={styles.card}>
+      <Text style={styles.name}>Name: {item.name}</Text>
+      <Text style={styles.quantity}>Quantity: {item.quantity}</Text>
+      <Text style={styles.price}>Price per ounce: ${item.price}</Text>
+    </View>;
+
+  return <FlatList data={data} renderItem={renderItem} keyExtractor={item => item.id} contentContainerStyle={styles.scrollStyles} />;
 };
 
 const styles = StyleSheet.create({
-  safeArea: {
-    height: '100%'
+  card: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  scrollStyles: {
+    paddingVertical: 20
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 10
+  },
+  quantity: {
+    fontSize: 16,
+    marginBottom: 5
+  },
+  price: {
+    fontSize: 16
   }
 });
-export default Untitled17;
+export default Card;
