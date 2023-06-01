084fbe5f7a7f254d7367a0c106eee448855f2a4d
Saad bin Abid <saad.abid@crowdbotics.com>
2023-06-01 11:28:07 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "qa-assessment-new-2-39768",
   "url": "https://qa-assessment-new-2-39768.botics.co",
   "api": "https://qa-assessment-new-2-39768.botics.co/api/v1",
-  "initialRoute": "login2"
+  "initialRoute": "Untitled15"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
