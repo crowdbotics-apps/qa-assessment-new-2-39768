ea57cc29d07aa042101a3ee04d3c7b8ad98390a2
Saad bin Abid <saad.abid@crowdbotics.com>
2023-06-01 11:47:21 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/login-2/index.js
+++ screens/login-2/index.js
@@ -1,197 +1,77 @@
-import { Pressable } from "react-native";
-import React, { useState } from "react";
-import { Text, View, TouchableOpacity, Image, TextInput, StyleSheet } from "react-native";
+import React from "react";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
 
 const Login2 = ({
   navigation
 }) => {
-  const [selected, setSelected] = useState(false);
-
-  const onPress = () => {
-    setSelected(!selected);
-  };
-
   return <View style={styles.container}>
-      <View style={styles.heading}>
-        <Text style={styles.headingText}>{"Login"}</Text>
-      </View>
-      <View>
-        <View style={styles.emailContainer}>
-          <Text style={styles.mr10}>Email address</Text>
-          <Input placeholder="Email" />
-        </View>
-        <View style={styles.mb20}>
-          <Text style={styles.mr10}>Password</Text>
-          <Input placeholder="Password" />
-        </View>
-        <View style={styles.forgotPassword}>
-          <View>
-            <CheckBox onPress={onPress} selected={selected} text="Remember me" />
-          </View>
-          <TouchableOpacity>
-            <Pressable onPress={() => {
-            navigation.navigate("forgotPassword");
-          }}>
-              <Text>Forgot Password?</Text>
-            </Pressable>
-          </TouchableOpacity>
-        </View>
-        {
-        /*         <View style={styles.loginContainer}>
-                <Button>Log In</Button>
-              </View> */
-      }
-      </View>
-      <Pressable onPress={() => {
-      navigation.navigate("signup");
-    }}>
-        <View style={styles.footerContainer}>
-          <Text style={styles.footerText}>Do not have an account?</Text>
-          <TouchableOpacity>
-            <Pressable onPress={() => {
-            navigation.navigate("signup");
-          }}>
-              <Text>Sign Up</Text>
-            </Pressable>
-          </TouchableOpacity>
-        </View>
-      </Pressable>
-      <View style={styles.DUqwmwLc}>
-        <Pressable onPress={() => {
-        navigation.navigate("Untitled21");
-      }}>
-          <Text style={styles.vkrKyqAb}>{"Login"}</Text>
-        </Pressable>
+      <Text style={styles.heading}>Login</Text>
+      <TextInput style={styles.input} placeholder="Email" />
+      <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} />
+      <TouchableOpacity>
+        <Text style={styles.forgotPassword}>Forgot password?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.loginButtonText}>Login</Text>
+      </TouchableOpacity>
+      <View style={styles.signupContainer}>
+        <Text style={styles.signupText}>Don't have an account? </Text>
+        <TouchableOpacity>
+          <Text style={styles.signupButton}>Signup.</Text>
+        </TouchableOpacity>
       </View>
     </View>;
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    height: "100%",
-    backgroundColor: "#fff"
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
   },
   heading: {
-    alignSelf: "center"
-  },
-  headingText: {
-    fontSize: 42,
-    fontWeight: "500",
-    position: "absolute",
-    top: 43,
-    left: -55
-  },
-  emailContainer: {
-    marginBottom: 10
-  },
-  mr10: {
-    marginRight: 10,
-    marginBottom: 10
-  },
-  mb20: {
+    fontSize: 24,
+    fontWeight: "bold",
     marginBottom: 20
   },
-  forgotPassword: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 40
-  },
-  footerContainer: {
-    alignSelf: "center",
-    display: "flex",
-    flexDirection: "row"
-  },
-  footerText: {
-    color: "#6B6B6B"
-  },
-  DUqwmwLc: {
-    height: 54,
-    width: 297,
-    backgroundColor: "#000",
-    borderRadius: 10,
-    color: "#777777",
-    position: "absolute",
-    top: 464,
-    left: 28
-  },
-  vkrKyqAb: {
-    width: 100,
+  input: {
+    width: "80%",
     height: 50,
-    lineHeight: 14,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    marginBottom: 10,
+    paddingLeft: 10
+  },
+  forgotPassword: {
+    marginTop: 10,
+    color: "blue"
+  },
+  loginButton: {
+    backgroundColor: "black",
+    width: "80%",
+    height: 50,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20
+  },
+  loginButtonText: {
+    color: "white",
     fontSize: 18,
-    borderRadius: 0,
-    color: "#fff",
-    fontWeight: "700",
-    position: "absolute",
-    top: 18,
-    left: 123
+    fontWeight: "bold"
+  },
+  signupContainer: {
+    flexDirection: "row",
+    marginTop: 20
+  },
+  signupText: {
+    fontSize: 16
+  },
+  signupButton: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "blue"
   }
 });
 export default Login2;
-
-const CheckBox = ({
-  selected,
-  onPress,
-  text
-}) => <TouchableOpacity onPress={onPress}>
-    <View style={checkBoxStyles.checkBoxContainer}>
-      <View style={checkBoxStyles.iconContainer}>
-        {selected && <Image source={require("./assets/checkbox.png")} style={checkBoxStyles.icon} />}
-      </View>
-      <Text style={checkBoxStyles.iconText}>{text}</Text>
-    </View>
-  </TouchableOpacity>;
-
-const checkBoxStyles = StyleSheet.create({
-  checkBoxContainer: {
-    display: "flex",
-    flexDirection: "row"
-  },
-  iconContainer: {
-    height: 18,
-    width: 18,
-    borderColor: "#000000",
-    borderWidth: 2,
-    borderRadius: 5,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  icon: {
-    height: 16,
-    width: 16
-  },
-  iconText: {
-    marginLeft: 10
-  }
-});
-
-const Input = props => {
-  return <View>
-      <TextInput style={textStyles.input} placeholder={props.placeholder} value={props.value} onChangeText={num => props.setValue(num)} placeholderTextColor="#ddd" editable={props.editable !== false} />
-      {props.errorText ? <Text style={textStyles.error}>{props.errorText}</Text> : null}
-    </View>;
-};
-
-const textStyles = StyleSheet.create({
-  input: {
-    backgroundColor: "#fff",
-    height: 53,
-    borderColor: "#C4C4C4",
-    color: "#000",
-    borderRadius: 10,
-    fontSize: 14,
-    borderWidth: 1,
-    paddingHorizontal: 10
-  },
-  error: {
-    fontSize: 13,
-    color: "#FA060D",
-    paddingTop: 8
-  }
-});
